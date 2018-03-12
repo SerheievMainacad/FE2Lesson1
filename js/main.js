cc13b9c0855511f2befc747e07b2db5a0b25813e
SerheievMainacad <serheiev@maincad.com>
2018-03-12 21:52:57 +0200

    oop in js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ console.log(Bob.human);
 Alex.sayHello();*/
 
 
-
+/*
 function Rectangle(w,h){
 	this.width = w;
 	this.height = h;
@@ -59,4 +59,180 @@ console.log(square1 + square2);
 console.log(square1 - square2);
 
 
+*/
+/*
+function rectangle(w,h){
 
+	this.width = w;
+	this.height = h;
+
+}
+
+
+rectangle.prototype.getArea = function(){
+		return this.width * this.height;
+	};
+
+
+rectangle.prototype.compareTo = function(otherObj){
+	if (this.getArea() > otherObj.getArea()){
+		return 1
+	}else if(this.getArea() < otherObj.getArea()){
+		return -1
+	}else{
+		return 0
+	}
+	
+}
+
+rectangle.prototype.toString = function(){
+	return "rectangle w: " + this.width + "rectangle h : " + this.height;
+}
+
+rectangle.prototype.valueOf = function(){
+	return this.getArea();
+}
+
+rectangle.prototype.equals = function(otherObj){
+	if(this.width == otherObj.width && this.height == otherObj.height){
+		return true
+	}
+	return false
+}
+
+var rect1 = new rectangle(100, 20);
+var rect2 = new rectangle(1000, 20);*/
+
+
+// var res = rect1.compareTo(rect2);
+// document.write(res)
+
+
+
+
+// > < >= <= == !=
+//if -----
+
+/*if(rect1 > rect2){
+	document.write('rect1 > rect2<br>')
+}
+
+if(rect1 < rect2){
+	document.write('rect1 < rect2<br>')
+}
+if(rect1 >= rect2){
+	document.write('rect1 >= rect2<br>')
+}
+
+if(rect1 <= rect2){
+	document.write('rect1 <= rect2<br>')
+}
+if(rect1.valueOf() == rect2.valueOf()){
+	document.write('rect1 == rect2<br>')
+}
+if(rect1.valueOf() != rect2.valueOf()){
+	document.write('rect1 != rect2<br>')
+}
+*/
+
+//incapsulation
+
+
+/*function myClass(){
+
+
+	function getSum(){
+		alert('private')
+	}
+	function sqr(){
+			
+	}
+
+	this.calculate = function(){
+		
+	};
+}
+
+var obj = new myClass();
+obj.publicMethod();
+*/
+
+
+
+
+
+
+//inheritance
+/*
+
+function human(name, smth){
+	this.name = name;
+	this.smth = smth;	
+}
+
+human.prototype.talk = function(){document.write('Hello my name is ' + this.name + '<br>')}
+
+function student(name, speciality){
+	this.name = name;
+	this.speciality = speciality;
+}
+
+function worker(name, speciality){
+	this.name = name;
+	this.speciality = speciality;
+}
+
+var human = new human('name', 'smth');
+
+student.prototype = human;
+worker.prototype = human;
+
+var Antony = new student('Antony', 'student');
+var Edgar = new worker('Edgar', 'worker');
+
+
+Antony.talk()
+document.write(Antony.speciality + '<br>');
+
+
+Edgar.talk()
+document.write(Edgar.speciality + '<br>');
+
+console.log(Edgar);
+
+*/
+
+//polimorphysm
+
+// function human(name){
+// 	this.name = name;
+// 	this.talk = function(){document.write('Hello my name is ' + this.name + '<br>')}
+// }
+
+
+// function student(name){
+// 	this.name = name;
+// 	this.academy = 'BrainAcademy'
+// }
+
+// function worker(name){
+// 	this.name = name;
+// 	this.speciality = 'Front-end developer';
+// 	this.talk = function(){document.write('I am ' + this.name + '<br>')}
+// }
+
+// var human = new human('name');
+
+// student.prototype = human;
+// worker.prototype = human;
+
+// var Antony = new student('Antony');
+// var Edgar = new worker('Edgar');
+
+
+// Antony.talk()
+// document.write(Antony.academy + '<br>');
+
+
+// Edgar.talk()
+// document.write(Edgar.speciality + '<br>');
